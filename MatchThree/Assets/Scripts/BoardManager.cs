bf7d898c079d7372393e07c5d739337dd43007b2
Syafiqjos <syafiqjos@gmail.com>
2021-05-11 14:35:55 +0700

    Add BoardManager

---
+++ MatchThree/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    #region Singleton
+    private static BoardManager _instance = null;
+    public static BoardManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<BoardManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: BoardManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+}
